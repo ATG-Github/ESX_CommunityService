e4d60c811fc8562b34ec0d5f327d5ed74bf5f3cb
Lautaro <covalautaro@gmail.com>
2020-05-27 10:07:16 -0300

    Added es translations

---
+++ locales/es.lua
@@ -0,0 +1,16 @@
+Locales ['en'] = {
+	['judge']              	= '[Gobierno] ',
+	['escape_attempt']     	= 'Debido a que intentaste escaparte de la prisión, deberás pasar más tiempo en prisión.',
+	['remaining_msg']      	= '¡Completa ~b~%s~s~ para ser liberado!',
+	['comserv_msg']       	= '%s fue sentenciado a %s años en prisión.',
+	['comserv_finished']  	= '%s ha cumplido su sentencia.',
+	['press_to_start']      = 'Presiona ~INPUT_CONTEXT~ para interactuar',
+	['system_msn']          = '^1SISTEMA ',
+	['invalid_player_id_or_actions']      = 'Has proporcionado una ID Inválida',
+	['insufficient_permissions']      = 'Lo siento, no tienes los permisos suficientes para hacer esto.',
+	['give_player_community']      = 'Sentencia a un Jugador a X años de Prisión',
+	['target_id']      = 'ID',
+	['action_count_suggested']      = 'Cantidad de Acciones/Años (Recomendado: 10-40)',
+	['invalid_player_id']      = 'ID Inválida!',
+	['unjail_people']      = 'Remueve la Sentencia del Jugador'
+}
